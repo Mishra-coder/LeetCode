f11cffd8c2fcece306734acbb3d6b85517550bce
Devendr Mishra <devendramishra495@gmail.com>
2026-04-13 22:01:10 +0530

    Time: 3 ms (86.86%), Space: 13.5 MB (73.31%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def diStringMatch(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        low, high = 0, len(s)
+        ans = []
+
+        for ch in s:
+            if ch == 'I':
+                ans.append(low)
+                low += 1
+            else:
+                ans.append(high)
+                high -= 1
+
+        ans.append(low)
+        return ans
+        
